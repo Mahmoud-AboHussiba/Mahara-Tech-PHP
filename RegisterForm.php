11caf9879ea81972b2204997969e1b4d4567c659
Mahmoud Badr <mahmoud.hussiba@gmail.com>
2024-11-22 13:38:46 +0200

    build HTMl form

---
+++ RegisterForm.php
@@ -0,0 +1,20 @@
+<html>
+    <body>
+        <form action="#" method="post">
+            <label for="name">Name</label>
+            <input type="text" name="name" id="name"/>
+            <br/>
+            <label for="email">Email</label>
+            <input type="email" name="email" id="email"/>
+            <br/>
+            <label for="password">Password</label>
+            <input type="password" name="password"/>
+            <br/>
+            <label for="gender">Gender</label>
+            <input type="radio" name="gender" value="male"/>Male
+            <input type="radio" name="gender" value="female"/>Female
+            <br/>
+            <input type="submit" name="submit" value="Register"/>
+        </form>
+    </body>
+</html>
