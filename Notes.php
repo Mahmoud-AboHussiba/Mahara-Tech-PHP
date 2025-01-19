666cda5f097ba12d07cea2b12775b46d4c5e9127
Mahmoud Badr <mahmoud.hussiba@gmail.com>
2025-01-19 07:50:07 +0200

    WIP

---
+++ Notes.php
@@ -0,0 +1,7 @@
+/*
+
+This file will contain notes for things that I have searched about it.
+
+1- For macOS Runing php pages from any folder if you have installed just php not also apache 
+    - go to your folder directory and run this command "php -S localhost:8000".
+*/
